72c18b98b8386d37f63a1925dcbb243edf784dfd
rikawav <rikawav@gmail.com>
2022-06-01 22:23:30 +0800

    更新subinfo panel

--- surge-panel-subinfo.js
+++ surge-panel-subinfo.js
@@ -1,51 +1,50 @@
-const Base64 = new Base64Code();
+const key = 'usage_query';
+const $ = new Env(key);
 
-/*
-https://manual.nssurge.com/scripting/common.html
-The $persistentStore is for persistent store: 
-$persistentStore.write(data<String>, [key<String>])
-$persistentStore.read([key<String>])
-ariport format:
-[{'tag':'xx','url':'http://xxx','style': 'text|percent|percentnum|percentandnum|percentauto'},{'tag':'xx','url':'http://xxx','style': 'text|percent|percentnum|percentandnum|percentauto'}]
-style:
-    text:机场tag➤  • ↑37.77 MB • ↓5.22 GB • 20GB
-    percent:机场tag➤ ⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆ 100%
-    percentnum:机场tag➤ ⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆ 30 GB
-    percentandnum:机场tag➤ ⋆⋆⋆⋆⋆⋆⋆⋆ 50% [10 GB]
-    percentauto:自动切换percent与percentandnum（用量<=50% 时使用percentandnum反之percent）
-⚠️注意：percent 风格显示方式受到机场tag字符串长度以及设备尺寸大小影响不建议使用，脚本调试仅针对iPhone12 pro适配
-*/
-const ariport_config_key = "ariport_amounts_config_key";
-const ariportValue = $persistentStore.read(ariport_config_key) || "";
-console.log(`ariport_config_key:${ariportValue}`);
+const KEY_DISABLED = `@ieeeee.${key}.disabled`;
+const KEY_STYLE = `@ieeeee.${key}.style`;
+const KEY_SUBSCRIBE = `@ieeeee.${key}.subscribe`;
+
+const disabled = $.getdata(KEY_DISABLED);
+const showStyle = $.getdata(KEY_STYLE) || 'text';
+const subscribeList = $.getdata(KEY_SUBSCRIBE) || '';
 
 /**
  * 构建出多个异步请求（每个异步请求都应该是一个Promise对象）
  */
-const ariport = JSON.parse(ariportValue) || [];
+//const ariport = JSON.parse(ariportValue) || [];
+const ariportSrc = subscribeList.match(/.*\|.*/g);
+if (!ariportSrc) {
+    $.msg('用量查询', '异常', '为获取到正确的订阅配置信息');
+    $.done(myResponse);
+}
+
+const ariport = ariportSrc.map((item) => item.split("|")).map(([k, v]) => { return { "tag": k, "url": v, "style": showStyle }; });
 const promises = ariport.map((item) => {
 
-    console.log(`[${item.tag}]用量信息查询中...`);
+    $.log(`[${item.tag}]用量信息查询中...`);
 
-    // 执行异步请求，返回一个 Promise 对象
+    const myRequest = {
+        url: item.url,
+        method: "HEAD",
+        headers: {
+            'User-Agent': "Quantumult%20X"
+        }
+    };
+
     return new Promise((resolve, reject) => {
         try {
-            let requestOpt = {
-                url: item.url,
-                headers: {
-                    'User-Agent': "Quantumult%20X"
-                },
-            };
-            $httpClient.head(requestOpt, function (error, resp, data) {
-                if (error === null) {
+            $.get(myRequest, function (err, resp, body) {
+                if (err === null) {
                     let header = Object.keys(resp.headers).find((key) => key.toLowerCase() === "subscription-userinfo");
                     resolve({ 'tag': item.tag, 'style': item.style, 'amounts': (header) ? resp.headers[header] : null });
                 } else {
+                    $.log(`[${item.tag}]获取用量信息异常...`);
                     resolve(null);
                 }
             });
         } catch (error) {
-            console.log(`[${item.tag}]用量信息查询出错...`);
+            $.log(`[${item.tag}]用量信息查询出错...`);
             resolve(null);
         }
     });
@@ -53,7 +52,7 @@ const promises = ariport.map((item) => {
 
 Promise.all(promises).then((result) => {
 
-    console.log(`[${ariport.length}]用量信息查询完成`);
+    $.log(`[${ariport.length}]用量信息查询完成`);
 
     const myResponseList = [];
 
@@ -84,7 +83,7 @@ Promise.all(promises).then((result) => {
                     let percent_amounts = (amounts / total) * 100;
                     let percent_remainder = 100 - percent_amounts;
 
-                    //屏幕可展示100% 的点数为45点[.]/或者32星[⋆]
+                    //屏幕可展示100% 的点数为45点[.]/或者40星[⋆]
                     let dotCount = (percent_remainder * 40) / 100;
                     for (let i = 0; i < dotCount; i++) {
                         tag.push(`.`);
@@ -111,24 +110,14 @@ Promise.all(promises).then((result) => {
                     tag.push(`↑${bytesToSize(tmp.upload)}`);
                     tag.push(`↓${bytesToSize(tmp.download)}`);
                     tag.push(`${bytesToSize(tmp.total - (tmp.download + tmp.upload))}`);
-                    //tag.push(`${bytesToSize(tmp.total)}`);
                     break;
             }
-
-            //魔戒•↑12.91MB•↓12.91MB•11.02GB • ☠︎ ⌘ ● ☛ ➤  
-            //∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙
-            //⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃
-            //▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎
-            //......................
-            //₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋₋
-            //······················
-            //⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆
 
             myResponseList.push(`${tag.join(separator)}`);
         }
     }
 
-    $done({
+    $.done({
         title: "订阅用量查询",
         //icon: "doc.text.magnifyingglass",
         //'icon-color': "#3498DB",
@@ -136,8 +125,8 @@ Promise.all(promises).then((result) => {
     });
 
 }).catch((error) => {
-    console.log(`Promise.all exception: ${error}`);
-    $done({
+    $.log(`Promise.all exception: ${error}`);
+    $.done({
         title: "订阅用量查询",
         //icon: "doc.text.magnifyingglass",
         //'icon-color': "#3498DB",
@@ -161,141 +150,5 @@ function formatTime(time) {
     return year + "年" + month + "月" + day + "日";
 }
 
-//比较完美的一款 base64 encode/decode 工具
-/*
- *  base64.js: https://github.com/dankogai/js-base64#readme
- *
- *  Licensed under the BSD 3-Clause License.
- *    http://opensource.org/licenses/BSD-3-Clause
- *
- *  References:
- *    http://en.wikipedia.org/wiki/Base64
- */
-//base64 完毕
-function Base64Code() {
-    // constants
-    var b64chars
-        = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-    var b64tab = function (bin) {
-        var t = {};
-        for (var i = 0, l = bin.length; i < l; i++) t[bin.charAt(i)] = i;
-        return t;
-    }(b64chars);
-    var fromCharCode = String.fromCharCode;
-    // encoder stuff
-    var cb_utob = function (c) {
-        if (c.length < 2) {
-            let cc = c.charCodeAt(0);
-            return cc < 0x80 ? c : cc < 0x800 ? (fromCharCode(0xc0 | (cc >>> 6)) + fromCharCode(0x80 | (cc & 0x3f))) : (fromCharCode(0xe0 | ((cc >>> 12) & 0x0f)) + fromCharCode(0x80 | ((cc >>> 6) & 0x3f)) + fromCharCode(0x80 | (cc & 0x3f)));
-        } else {
-            let cc = 0x10000 + (c.charCodeAt(0) - 0xD800) * 0x400 + (c.charCodeAt(1) - 0xDC00);
-            return (fromCharCode(0xf0 | ((cc >>> 18) & 0x07)) + fromCharCode(0x80 | ((cc >>> 12) & 0x3f)) + fromCharCode(0x80 | ((cc >>> 6) & 0x3f)) + fromCharCode(0x80 | (cc & 0x3f)));
-        }
-    };
-    var re_utob = /[\uD800-\uDBFF][\uDC00-\uDFFFF]|[^\x00-\x7F]/g;
-    var utob = function (u) {
-        return u.replace(re_utob, cb_utob);
-    };
-    var cb_encode = function (ccc) {
-        var padlen = [0, 2, 1][ccc.length % 3],
-            ord = ccc.charCodeAt(0) << 16
-                | ((ccc.length > 1 ? ccc.charCodeAt(1) : 0) << 8)
-                | ((ccc.length > 2 ? ccc.charCodeAt(2) : 0)),
-            chars = [
-                b64chars.charAt(ord >>> 18),
-                b64chars.charAt((ord >>> 12) & 63),
-                padlen >= 2 ? '=' : b64chars.charAt((ord >>> 6) & 63),
-                padlen >= 1 ? '=' : b64chars.charAt(ord & 63)
-            ];
-        return chars.join('');
-    };
-    var btoa = function (b) {
-        return b.replace(/[\s\S]{1,3}/g, cb_encode);
-    };
-    // var _encode = function(u) {
-    //  var isUint8Array = Object.prototype.toString.call(u) === '[object Uint8Array]';
-    //  return isUint8Array ? u.toString('base64')
-    //    : btoa(utob(String(u)));
-    // }
-    this.encode = function (u) {
-        var isUint8Array = Object.prototype.toString.call(u) === '[object Uint8Array]';
-        return isUint8Array ? u.toString('base64')
-            : btoa(utob(String(u)));
-    };
-    var uriencode = function (u, urisafe) {
-        return !urisafe ? _encode(u)
-            : _encode(String(u)).replace(/[+\/]/g, function (m0) {
-                return m0 == '+' ? '-' : '_';
-            }).replace(/=/g, '');
-    };
-    var encodeURI = function (u) { return uriencode(u, true); };
-    // decoder stuff
-    var re_btou = /[\xC0-\xDF][\x80-\xBF]|[\xE0-\xEF][\x80-\xBF]{2}|[\xF0-\xF7][\x80-\xBF]{3}/g;
-    var cb_btou = function (cccc) {
-        switch (cccc.length) {
-            case 4:
-                var cp = ((0x07 & cccc.charCodeAt(0)) << 18)
-                    | ((0x3f & cccc.charCodeAt(1)) << 12)
-                    | ((0x3f & cccc.charCodeAt(2)) << 6)
-                    | (0x3f & cccc.charCodeAt(3)),
-                    offset = cp - 0x10000;
-                return (fromCharCode((offset >>> 10) + 0xD800) + fromCharCode((offset & 0x3FF) + 0xDC00));
-            case 3:
-                return fromCharCode(
-                    ((0x0f & cccc.charCodeAt(0)) << 12)
-                    | ((0x3f & cccc.charCodeAt(1)) << 6)
-                    | (0x3f & cccc.charCodeAt(2))
-                );
-            default:
-                return fromCharCode(
-                    ((0x1f & cccc.charCodeAt(0)) << 6)
-                    | (0x3f & cccc.charCodeAt(1))
-                );
-        }
-    };
-    var btou = function (b) {
-        return b.replace(re_btou, cb_btou);
-    };
-    var cb_decode = function (cccc) {
-        var len = cccc.length,
-            padlen = len % 4,
-            n = (len > 0 ? b64tab[cccc.charAt(0)] << 18 : 0)
-                | (len > 1 ? b64tab[cccc.charAt(1)] << 12 : 0)
-                | (len > 2 ? b64tab[cccc.charAt(2)] << 6 : 0)
-                | (len > 3 ? b64tab[cccc.charAt(3)] : 0),
-            chars = [
-                fromCharCode(n >>> 16),
-                fromCharCode((n >>> 8) & 0xff),
-                fromCharCode(n & 0xff)
-            ];
-        chars.length -= [0, 0, 2, 1][padlen];
-        return chars.join('');
-    };
-    var _atob = function (a) {
-        return a.replace(/\S{1,4}/g, cb_decode);
-    };
-    var atob = function (a) {
-        return _atob(String(a).replace(/[^A-Za-z0-9\+\/]/g, ''));
-    };
-    // var _decode = buffer ?
-    //  buffer.from && Uint8Array && buffer.from !== Uint8Array.from
-    //  ? function(a) {
-    //    return (a.constructor === buffer.constructor
-    //        ? a : buffer.from(a, 'base64')).toString();
-    //  }
-    //  : function(a) {
-    //    return (a.constructor === buffer.constructor
-    //        ? a : new buffer(a, 'base64')).toString();
-    //  }
-    //  : function(a) { return btou(_atob(a)) };
-    var _decode = function (u) {
-        return btou(_atob(u));
-    };
-
-    this.decode = function (a) {
-        return _decode(
-            String(a).replace(/[-_]/g, function (m0) { return m0 == '-' ? '+' : '/'; })
-                .replace(/[^A-Za-z0-9\+\/]/g, '')
-        ).replace(/&gt;/g, ">").replace(/&lt;/g, "<");
-    };
-}
+// prettier-ignore
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}isShadowrocket(){return"undefined"!=typeof $rocket}isStash(){return"undefined"!=typeof $environment&&$environment["stash-version"]}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),n={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(n,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){if(t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let s=require("iconv-lite");this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:i,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:i,statusCode:r,headers:o,rawBody:h},s.decode(h,this.encoding))},t=>{const{message:i,response:r}=t;e(i,r,r&&s.decode(r.rawBody,this.encoding))})}}post(t,e=(()=>{})){const s=t.method?t.method.toLocaleLowerCase():"post";if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient[s](t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method=s,this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let i=require("iconv-lite");this.initGotEnv(t);const{url:r,...o}=t;this.got[s](r,o).then(t=>{const{statusCode:s,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:s,statusCode:r,headers:o,rawBody:h},i.decode(h,this.encoding))},t=>{const{message:s,response:r}=t;e(s,r,r&&i.decode(r.rawBody,this.encoding))})}}time(t,e=null){const s=e?new Date(e):new Date;let i={"M+":s.getMonth()+1,"d+":s.getDate(),"H+":s.getHours(),"m+":s.getMinutes(),"s+":s.getSeconds(),"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(s.getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in i)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?i[e]:("00"+i[e]).substr((""+i[e]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl,i=t["update-pasteboard"]||t.updatePasteboard;return{"open-url":e,"media-url":s,"update-pasteboard":i}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
