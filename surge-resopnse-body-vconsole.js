811b919703610336d2dc4b6cc51c9fb6a201cdea
Charlesli <webfans@live.cn>
2023-02-28 12:25:41 +0800

    更新 surge-resopnse-body-vconsole.js

--- surge-resopnse-body-vconsole.js
+++ surge-resopnse-body-vconsole.js
@@ -1,3 +1,4 @@
+console.log('vConsole injected...');
 let vConsole = `<script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js"></script><script>var vConsole = new window.VConsole();</script>`;
 $done({
     body: $response.body+vConsole
